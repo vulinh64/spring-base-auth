8d1ffdcf3fba46ab7d243cba8204e85c82c5653f
Linh Nguyen <nguyenvulinh64@outlook.com>
2026-05-02 21:52:59 +0700

    Something something something something

--- src/main/java/com/vulinh/configuration/InterServiceAuthenticator.java
+++ src/main/java/com/vulinh/configuration/InterServiceAuthenticator.java
@@ -1,10 +1,7 @@
 package com.vulinh.configuration;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public interface InterServiceAuthenticator {
 
